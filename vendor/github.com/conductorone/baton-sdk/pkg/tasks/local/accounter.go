6a42a6cf7fbfc91f390c46cf4047ae5be95a4848
Alejandro Bernal <jbernalxyz@gmail.com>
2025-11-11 18:03:43 -0500

    update baton-sdk

--- vendor/github.com/conductorone/baton-sdk/pkg/tasks/local/accounter.go
+++ vendor/github.com/conductorone/baton-sdk/pkg/tasks/local/accounter.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 	"time"
 
+	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/protobuf/types/known/structpb"
 
 	v1 "github.com/conductorone/baton-sdk/pb/c1/connectorapi/baton/v1"
@@ -33,14 +34,17 @@ func (m *localAccountManager) ShouldDebug() bool {
 func (m *localAccountManager) Next(ctx context.Context) (*v1.Task, time.Duration, error) {
 	var task *v1.Task
 	m.o.Do(func() {
-		task = &v1.Task{
-			TaskType: &v1.Task_CreateAccount{},
-		}
+		task = v1.Task_builder{
+			CreateAccount: &v1.Task_CreateAccountTask{},
+		}.Build()
 	})
 	return task, 0, nil
 }
 
 func (m *localAccountManager) Process(ctx context.Context, task *v1.Task, cc types.ConnectorClient) error {
+	ctx, span := tracer.Start(ctx, "localAccountManager.Process", trace.WithNewRoot())
+	defer span.End()
+
 	accountManager := provisioner.NewCreateAccountManager(cc, m.dbPath, m.login, m.email, m.profile)
 
 	err := accountManager.Run(ctx)
