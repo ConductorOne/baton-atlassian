25f2a2fb394355d7c516f5a0b358e5c44e208a40
Alejandro Bernal <33394999+aldevv@users.noreply.github.com>
2025-11-26 17:39:53 -0500

    containerize connector (#23)
    * containerize connector
    
    * add config
    
    * add dockerfile
    
    * update release.yaml
    
    * update config vars usage

--- cmd/baton-atlassian/main.go
+++ cmd/baton-atlassian/main.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/conductorone/baton-atlassian/pkg/config"
 	connectorSchema "github.com/conductorone/baton-atlassian/pkg/connector"
-	"github.com/conductorone/baton-sdk/pkg/config"
+	sdkConfig "github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
 	"github.com/conductorone/baton-sdk/pkg/field"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
-	"github.com/spf13/viper"
 	"go.uber.org/zap"
 )
 
@@ -20,13 +20,11 @@ var version = "dev"
 func main() {
 	ctx := context.Background()
 
-	_, cmd, err := config.DefineConfiguration(
+	_, cmd, err := sdkConfig.DefineConfiguration(
 		ctx,
 		"baton-atlassian",
 		getConnector,
-		field.Configuration{
-			Fields: ConfigurationFields,
-		},
+		config.Configuration,
 	)
 	if err != nil {
 		_, err := fmt.Fprintln(os.Stderr, err.Error())
@@ -48,20 +46,17 @@ func main() {
 	}
 }
 
-func getConnector(ctx context.Context, v *viper.Viper) (types.ConnectorServer, error) {
+func getConnector(ctx context.Context, cfg *config.Atlassian) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
 
-	if err := ValidateConfig(v); err != nil {
+	if err := field.Validate(config.Configuration, cfg); err != nil {
 		return nil, err
 	}
 
-	accessToken := v.GetString(accessTokenField.FieldName)
-	organizationID := v.GetString(organizationIDField.FieldName)
-
 	connectorBuilder, err := connectorSchema.New(
 		ctx,
-		accessToken,
-		organizationID,
+		cfg.AccessToken,
+		cfg.OrganizationId,
 	)
 
 	if err != nil {
